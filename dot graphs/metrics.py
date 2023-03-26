1b193569704084abd8874e216f4ee0ac82e26304
rohith <cheryalarohith@gmail.com>
2023-03-26 16:46:56 +0530

    changed key value from node to (servName,opName)

--- dot graphs/metrics.py
+++ dot graphs/metrics.py
@@ -24,45 +24,45 @@ graph.assignLevels()
 def getChildrenErrorDict(node:GraphNode):
     di = {}
     for child in node.children.keys():
+        serviceName = graph.processName[child.pid]
+        key = (serviceName,child.opName)
         if child.errorFlag == True:
-            di[child] = 1
+            di[key] = 1
         else:
-            di[child] = 0
-    
+            di[key] = 0
     return di
     
 def helper(node:GraphNode,d):
     children = node.children.keys()
     # serviceName = self.processName[root.pid]
     serviceName = graph.processName[node.pid]
-    key = (serviceName,node.opName, node.sid)
+    key = (serviceName,node.opName)
     if d.get(key) is None:
         d[key] = [0,0,0,0,{}]
-    else:
-        if node.hasErrorChild == True and node.errorFlag == True:
-            value = d[key] 
-            value[0] += 1
-            value[1] += 0
-            value[2] += 1
-            value[3] += 0
-        if node.hasErrorChild == True and node.errorFlag == False:
-            value = d[key] 
-            value[0] += 1
-            value[1] += 1
-            value[2] += 0
-            value[3] += 0
-        if node.hasErrorChild == False and node.errorFlag == True:
-            value = d[key] 
-            value[0] += 0
-            value[1] += 0
-            value[2] += 0
-            value[3] += 1
-        if node.hasErrorChild == False and node.errorFlag == False:
-            value = d[key] 
-            value[0] += 0
-            value[1] += 0
-            value[2] += 0
-            value[3] += 0
+    if node.hasErrorChild == True and node.errorFlag == True:
+        value = d[key] 
+        value[0] += 1
+        value[1] += 0
+        value[2] += 1
+        value[3] += 0
+    if node.hasErrorChild == True and node.errorFlag == False:
+        value = d[key] 
+        value[0] += 1
+        value[1] += 1
+        value[2] += 0
+        value[3] += 0
+    if node.hasErrorChild == False and node.errorFlag == True:
+        value = d[key] 
+        value[0] += 0
+        value[1] += 0
+        value[2] += 0
+        value[3] += 1
+    if node.hasErrorChild == False and node.errorFlag == False:
+        value = d[key] 
+        value[0] += 0
+        value[1] += 0
+        value[2] += 0
+        value[3] += 0
     
     d[key][4] = getChildrenErrorDict(node)
 
@@ -78,7 +78,7 @@ sorted_d = dict(sorted(d.items(), key = lambda x:x[1][0],reverse=True))
 
 with open('./dict.txt', 'w+') as f:
     for key, value in sorted_d.items(): 
-        f.write('%s:%s\n' % (f'{key[0]} {key[1]} {key[2]}', f'{value[0]} {value[1]} {value[2]} {value[3]} {value[4]}'))
+        f.write('%s:%s\n' % (f'{key[0]} {key[1]}', f'{value[0]} {value[1]} {value[2]} {value[3]} {value[4]}'))
 
 
 
