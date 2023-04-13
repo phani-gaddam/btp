97a02c3b2b15453dc20ac62170df702ab267e0ef
Phani Teja Gaddam <phani@Phanis-MacBook-Air.local>
2023-04-13 21:14:44 +0530

    updated merge

--- merge.py
+++ merge.py
@@ -28,38 +28,38 @@ def help(graph):
 
 
 
-def merge_graphs(list_of_graphs):
-    final_graph = list_of_graphs[0]
+# def merge_graphs(list_of_graphs):
+#     final_graph = list_of_graphs[0]
 
-    to_visit = []
-    node = final_graph.rootNode
-    to_visit.append(node)
+#     to_visit = []
+#     node = final_graph.rootNode
+#     to_visit.append(node)
   
-    node_serviceName = final_graph.processName[node.pid]
-    while(len(to_visit) !=0):
-        children = list(node.children.keys())
-        for graph in  list_of_graphs[1:]:
-            for new_node in graph.nodeHT.values():
-                new_nodeserviceName = graph.processName[new_node.pid]
-                if node_serviceName == new_nodeserviceName and node.opName == new_node.opName and node.level == new_node.level:
-                    for key in children:
-                        child1 = key
-                        child1_sName = final_graph.processName[child1.pid]
-                        for child2 in new_node.children:
-                            child2_sName = graph.processName[child2.pid]
-                            if child1.opName == child2.opName and child1_sName == child2_sName and child1.level == child2.level:
-                                print("+")
-                                print(child1)
-                                print(child2)
-                                continue
+#     node_serviceName = final_graph.processName[node.pid]
+#     while(len(to_visit) !=0):
+#         children = list(node.children.keys())
+#         for graph in  list_of_graphs[1:]:
+#             for new_node in graph.nodeHT.values():
+#                 new_nodeserviceName = graph.processName[new_node.pid]
+#                 if node_serviceName == new_nodeserviceName and node.opName == new_node.opName and node.level == new_node.level:
+#                     for key in children:
+#                         child1 = key
+#                         child1_sName = final_graph.processName[child1.pid]
+#                         for child2 in new_node.children:
+#                             child2_sName = graph.processName[child2.pid]
+#                             if child1.opName == child2.opName and child1_sName == child2_sName and child1.level == child2.level:
+#                                 print("+")
+#                                 print(child1)
+#                                 print(child2)
+#                                 continue
                                 
-                            else:
-                                print ("-")
-                                node.children[child2] = True
+#                             else:
+#                                 print ("-")
+#                                 node.children[child2] = True
 
-                                final_graph.processName[child2.pid] = child2_sName
-                                # final_graph.nodeHT[]
+#                                 final_graph.processName[child2.pid] = child2_sName
+#                                 # final_graph.nodeHT[]
                 
-        to_visit.pop(0)
-    help(final_graph)
-    print(final_graph.rootNode.children)
+#         to_visit.pop(0)
+#     help(final_graph)
+#     print(final_graph.rootNode.children)
