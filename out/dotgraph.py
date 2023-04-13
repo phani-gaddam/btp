d49e2b153e88487ac0ba26ed06a325e10cd874ff
rohith <cheryalarohith@gmail.com>
2023-04-14 02:14:39 +0530

    graphviz in html using d3-graphviz

---
+++ out/dotgraph.py
@@ -0,0 +1,22 @@
+import json
+import graphviz
+
+g = graphviz.Digraph('G', filename='final.gv', format="svg")
+
+
+path = r"/home/rohith/code/btp/out/dict.json"
+
+data = json.load(open(path))
+
+print(data)
+
+for k,v in data.items():
+    g.node(name=k,label=k)
+
+for k,v in data.items():
+    for n,val in v[4].items():
+        g.edge(k,n,weight=str(val),penwidth=str(val),label=str(val))
+
+
+
+g.render(outfile="final.svg")
